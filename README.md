bc0c420d1c467eb6444bd68848cc658227d23674
Kaic Nunes <kaicmurilo@live.com>
2023-05-20 11:04:44 -0400

    Inserindo cotação de cripto e melhorando estrutura do código

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 ﻿# Telegram Bot com Interações Simples
+
 Este é um código em Node.js que implementa um bot do Telegram com interações simples. O bot recebe mensagens dos usuários e responde com base nas opções selecionadas.
 
 ## Pré-requisitos
+
 Node.js instalado
 Conta no Twilio para envio de mensagens SMS (Opcional)
 Conta no Telegram para criar um bot e obter um token de acesso
+
 ## Configuração
+
 Clone o repositório ou crie um novo arquivo para o código.
 Execute o comando npm install para instalar as dependências necessárias: dotenv, twilio e node-telegram-bot-api.
 Crie um arquivo .env na raiz do projeto e configure as seguintes variáveis de ambiente:
@@ -21,13 +25,31 @@ TELEGRAM_TOKEN=SEU_TELEGRAM_TOKEN
 Certifique-se de substituir os valores SEU_TWILIO_ACCOUNT_SID, SEU_TWILIO_AUTH_TOKEN, SEU_NUMERO_DE_TELEFONE_DO_WHATSAPP, SEU_NUMERO_DE_TELEFONE_PARA_RECEBER_SMS e SEU_TELEGRAM_TOKEN pelas suas próprias informações.
 
 ## Uso
-Execute o código usando o comando *node bot.js*.
+
+Faça o download do código ou o clone:
+
+´´´bash
+git clone https://github.com/kaicmurilo/botcreation
+´´´
+
+Instale as depêndencias:
+
+´´´bash
+npm install
+´´´
+
+Execute o código usando o comando:
+
+´´´bash
+node bot.js
+´´´
 
 O bot enviará uma mensagem SMS para o número configurado informando que está ativo.
 
 No Telegram, inicie uma conversa com o bot e envie mensagens de texto para interagir com ele. O bot responderá com base nas opções selecionadas ou saudará o usuário com base no horário atual.
 
 ## Funcionalidades
+
 O bot envia uma mensagem SMS para informar que está ativo.
 O bot responde às mensagens do Telegram com saudações apropriadas com base no horário atual.
 O bot oferece opções para o usuário selecionar e responde com base nas opções escolhidas.
@@ -35,6 +57,15 @@ O bot lida com mensagens de áudio informando que ainda não há processamento p
 Fique à vontade para modificar e expandir o código para adicionar mais recursos e interações ao bot do Telegram.
 
 ## Observações
+
 Certifique-se de ter configurado corretamente as credenciais do Twilio e do Telegram.
 Lembre-se de tratar adequadamente os erros e exceções para garantir o bom funcionamento do bot.
 Esse é apenas um exemplo básico de interação com um bot do Telegram. Sinta-se à vontade para adaptar o código e adicionar novas funcionalidades de acordo com suas necessidades.
+
+## Requisições Externas
+
+- twilioClient
+- telegramBot
+- Coingecko
+
+Aplicação necessita de conexão com a internet.
--- bot.js
+++ bot.js
@@ -1,8 +1,21 @@
-require("dotenv").config();
-// Importação das bibliotecas
 const twilio = require("twilio");
 const TelegramBot = require("node-telegram-bot-api");
 const moment = require("moment");
+const axios = require("axios");
+require("dotenv").config();
+
+// Função para obter as cotações das moedas
+async function obterCotacoes() {
+  try {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=brl"
+    );
+
+    return response.data;
+  } catch (error) {
+    return "Erro ao obter as cotações:", error.message;
+  }
+}
 
 // Configuração das credenciais
 const twilioClient = twilio(
@@ -13,7 +26,8 @@ const telegramBot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
   polling: true,
 });
 
-// Evento para enviar sms com twilio
+// Evento para enviar SMS com Twilio (descomente se quiser utilizar)
+/*
 twilioClient.messages
   .create({
     from: process.env.WHATSAPP_PHONE_NUMBER,
@@ -22,32 +36,30 @@ twilioClient.messages
   })
   .then((message) => console.log(message.sid))
   .catch((err) => console.error(err));
+*/
 
 // Evento para receber mensagens do Telegram
-telegramBot.on("message", (msg) => {
+telegramBot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
   const messageAudio = msg.voice;
   const nomeMensageiro = msg.chat.first_name + " " + msg.chat.last_name;
 
-  console.log(msg);
   if (messageAudio) {
     telegramBot.sendMessage(
       chatId,
-      `Ainda não temos processamento para Audio!`
+      `Ainda não temos processamento para áudio!`
     );
+    return;
   }
 
-  if (!["1", "2", "3"].includes(msg.text)) {
-    // Obter a hora atual
+  if (!["1", "2", "3", "4"].includes(messageText)) {
     const currentTime = moment();
-
-    // Definir as faixas de horário para as saudações
     const morningTime = moment("06:00", "HH:mm");
     const afternoonTime = moment("12:00", "HH:mm");
     const eveningTime = moment("18:00", "HH:mm");
     let greetingMessage = "";
-    // Comparar a hora atual com as faixas de horário
+
     if (currentTime.isBetween(morningTime, afternoonTime)) {
       greetingMessage = "Bom dia!";
     } else if (currentTime.isBetween(afternoonTime, eveningTime)) {
@@ -58,18 +70,28 @@ telegramBot.on("message", (msg) => {
 
     telegramBot.sendMessage(
       chatId,
-      `${greetingMessage} ${nomeMensageiro} \n Digite a opção desejada: \n 1 - Valor do Dólar \n 2 - Nome do Desenvolvedor do Bot \n 3 - Linguagem de Desenvolvimento`
+      `${greetingMessage} ${nomeMensageiro} \nDigite a opção desejada: \n1 - Valor do Dólar \n2 - Nome do Desenvolvedor do Bot \n3 - Linguagem de Desenvolvimento \n4 - Cotação Bitcoin e Ethereum`
     );
+    return;
   }
 
   const options = {
     1: "Valor do dólar: A implementar.",
     2: "Desenvolvedor Responsável: Kaic Murilo Nunes.",
     3: "Linguagem de Desenvolvimento: Node.js / JavaScript.",
+    4: "Aguarde enquanto as cotações são obtidas...",
   };
-  if (options[msg.text]) {
-    telegramBot.sendMessage(chatId, `Você Digitou: ${messageText}`);
-    telegramBot.sendMessage(chatId, options[msg.text]);
+
+  if (messageText === "4") {
+    const cotacoes = await obterCotacoes();
+
+    if (cotacoes) {
+      options[4] = `Bitcoin: R$ ${cotacoes.bitcoin.brl} \nEthereum: R$ ${cotacoes.ethereum.brl} \nValores obtidos em tempo real do Coingecko.`;
+    }
   }
-  console.log(msg.text);
+
+  if (options[messageText]) {
+    telegramBot.sendMessage(chatId, `Você Digitou: ${messageText}`);
+    telegramBot.sendMessage(chatId, options[messageText]);
+  }
 });
